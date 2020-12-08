72edcfaf80c9d172a23c7b49a4fb4a09ea3b18bf
us3soap <kevin.valette@gmail.com>
2020-12-08 16:01:38 +0100

    With guard

--- CellarManagement/ViewController.swift
+++ CellarManagement/ViewController.swift
@@ -30,23 +30,22 @@ class ViewController: UIViewController {
     }
     
     @IBAction func save(_ sender: UIButton) {
+    
+        guard let name = nameTextField.text, !name.isEmpty else { return }
         
-        if let name = nameTextField.text,
-           let castleName = castleNameTextField.text,
-           let castleDepartement = castleDepartementTextField.text,
-           let castleDepartementInt = Int(castleDepartement),
-           let year = yearTextField.text,
-           let yearInt = Int(year),
-           let price = priceTextField.text,
-           let priceDouble = Double(price),
-           let comment = commentTextField.text {
-            
-            let wine = Wine(name: name, castle: Castle(name: castleName, department: castleDepartementInt), year: yearInt, isOrganic: isOrganicSwitch.isOn, price: priceDouble, urlImage: nil, grapeVarities: [], medal: nil,rating: 10.0, comment: comment)
-            
-            print(wine.description())
-            
-        }
+        guard let castleName = castleNameTextField.text, !name.isEmpty else { return }
+       
+        guard let castleDepartementString = castleDepartementTextField.text, let castleDepartement = Int( castleDepartementString) else { return }
         
+        guard let yearString = yearTextField.text, let year = Int(yearString) else { return }
+        
+        guard let priceString = priceTextField.text, let price = Double(priceString) else { return }
+        
+        guard let comment = commentTextField.text else { return }
+        
+        let wine = Wine(name: name, castle: Castle(name: castleName, department: castleDepartement), year: year, isOrganic: isOrganicSwitch.isOn, price: price, urlImage: nil, grapeVarities: [], medal: nil,rating: 10.0, comment: comment)
+        
+        print(wine.description())
 
     }
     
