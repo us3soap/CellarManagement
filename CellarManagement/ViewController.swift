57ff32fd6c0a8d7d76587a60c9393d7e9103607e
us3soap <kevin.valette@gmail.com>
2020-12-08 12:35:00 +0100

    Initial Commit

---
+++ CellarManagement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CellarManagement
+//
+//  Created by Kevin Valette on 08/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
