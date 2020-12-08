f6508f3a72d90a23796eeeca077d9605dbb153cd
us3soap <kevin.valette@gmail.com>
2020-12-08 14:24:57 +0100

    Demo outlet

--- CellarManagement/ViewController.swift
+++ CellarManagement/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var sayHelloBtn: UIButton!
+    @IBOutlet weak var stateSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.stateSwitch.isOn = false
     }
 
-
+    @IBAction func sayHello(_ sender: UIButton) {
+        //self.view.backgroundColor = UIColor.blue
+        self.helloLabel.text = "Hello \(nameTextField?.text ?? "") !"
+        self.sayHelloBtn.isEnabled = false
+        self.stateSwitch.isOn = false
+        
+    }
+    
+    @IBAction func changeState(_ sender: UISwitch) {
+        if(self.stateSwitch.isOn){
+            self.sayHelloBtn.isEnabled = true
+        }
+    }
 }
 
