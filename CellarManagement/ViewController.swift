606c979882cb0a15370264b392bccf588c1630bd
us3soap <kevin.valette@gmail.com>
2020-12-09 15:44:57 +0100

    change guard

--- CellarManagement/ViewController.swift
+++ CellarManagement/ViewController.swift
@@ -39,22 +39,18 @@ class ViewController: UIViewController {
     
     @IBAction func save(_ sender: UIButton) {
     
-        guard let name = nameTextField.text, !name.isEmpty else { return }
+        guard let name = nameTextField.text,
+              !name.isEmpty,
+              let castleName = castleNameTextField.text,
+              !name.isEmpty,
+              let castleDepartement = castleDepartementTextField.intValue,
+              let year = yearTextField.intValue,
+              let price = priceTextField.doubleValue,
+              let comment = commentTextField.text
+        else { return }
         
-        guard let castleName = castleNameTextField.text, !name.isEmpty else { return }
-       
-        guard let castleDepartement = castleDepartementTextField.intValue else { return }
         
-        guard let year = yearTextField.intValue else { return }
-        
-        guard let price = priceTextField.doubleValue else { return }
-        
-        guard let comment = commentTextField.text else { return }
-                
-        guard let medal = Wine.Medal.init(rawValue: medalSegmented.selectedSegmentIndex) else { return }
-        print(medal)
-        
-        let wine = Wine(name: name, castle: Castle(name: castleName, department: castleDepartement), year: year, isOrganic: isOrganicSwitch.isOn, price: price, urlImage: nil, grapeVarities: [], medal: medal, rating: 10.0, comment: comment)
+        let wine = Wine(name: name, castle: Castle(name: castleName, department: castleDepartement), year: year, isOrganic: isOrganicSwitch.isOn, price: price, urlImage: nil, grapeVarities: [], medal: Wine.Medal.init(rawValue: medalSegmented.selectedSegmentIndex), rating: 10.0, comment: comment)
         
         print(wine.description())
 
